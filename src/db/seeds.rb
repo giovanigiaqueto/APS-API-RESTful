431894ffab980b6f23bd0cbf5db1918035795d83
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2023-10-17 18:24:14 -0300

    Carregamento de valores no banco através da tarefa 'db:seeds'
    modificação do arquivo "src/db/seeds.rb", usado pela tarefa 'db:seeds',
    para carregar usuários pré-definidos no banco de dados através da tarefa
    'db:seeds:usuarios'

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+module SeedsHelper
+  BANNER_SIZE = 80
+
+  def self.banner(texto, borda = '=', largura = nil)
+    largura = SeedsHelper::BANNER_SIZE
+    [borda * largura, texto.center(largura), borda * largura].join("\n")
+  end
+
+  def self.banner_tarefa(tarefa, largura = nil)
+    self.banner("executando tarefa Rake '#{tarefa}'", borda = '-', largura = largura)
+  end
+end
+
+if Rake::Task.task_defined?('db:seed:usuarios')
+  puts(
+    "\n",
+    SeedsHelper::banner("carregando usuários pré-definidos no banco de dados"),
+    "\n",
+    SeedsHelper::banner_tarefa('db:seed:usuarios'),
+    "\n"
+  )
+  Rake.application['db:seed:usuarios'].invoke
+end
