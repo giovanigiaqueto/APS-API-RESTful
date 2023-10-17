099edcb1e0bbe59b626264c99e4126a19500c2c4
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2023-10-17 16:53:41 -0300

    Correção na validação de usuários
    correção de erro nas validações do modelo User do arquivo
    "src/app/models/user.rb" que só consideravam usuários com permissão de
    escrita e de administração (admin) como válidos, impedindo a criação de
    usuários comuns sem permissão de escrita ou administração

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
   validates :name, allow_blank: false, uniqueness: true, presence: true
-  validates :allow_write, presence: true
-  validates :admin, presence: true
+  validate :allow_write_cannot_be_nil, :admin_cannot_be_nil
+
+  def allow_write_cannot_be_nil
+    # valida se o atributo 'allow_write' não é 'nil'
+    if allow_write.nil?
+      errors.add(:allow_write, "can't be nil")
+    end
+  end
+
+  def admin_cannot_be_nil
+    # valida se o atributo 'admin' não é 'nil'
+    if admin.nil?
+      errors.add(:admin, "can't be nil")
+    end
+  end
 
   before_validation do |user|
     user.allow_write = true if user.admin?
