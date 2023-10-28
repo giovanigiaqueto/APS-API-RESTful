060313dccce9ecd092d92796bfde8409964fb9b3
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2023-10-28 19:24:55 -0300

    Recuperação de erro padronizada em partes dos controladores
    recuperação de erros como "ActiveRecord::RecordNotFound" e conversão em
    resposta de erro HTTP apropriada, simplificando algumas partes do código
    dos controladores

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::API
   include Auth::Http
   include Auth::Jwt
+
+  rescue_from ActiveRecord::RecordNotFound, with: :recurso_nao_encontrado
+  rescue_from ActionController::ParameterMissing, with: :parametros_faltantes
+  rescue_from Auth::Unauthorized, with: :nao_autorizado
+
+  def recurso_nao_encontrado
+    erro_request("Not Found", "resource not found", :not_found)
+  end
+
+  def nao_autorizado
+    request_nao_autorizada("authentication required")
+  end
+
+  def parametros_faltantes
+    request_invalida("missing parameters")
+  end
 end
 
